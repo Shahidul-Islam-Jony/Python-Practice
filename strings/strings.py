08126c51d2f634e1a5e4b5a071dd7d9a3decaa29
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-06-02 17:10:45 +0600

    Python variables type and String practised

---
+++ strings/strings.py
@@ -0,0 +1,23 @@
+# string operations
+
+first_name = "Shahidul"
+last_name = "Islam"
+age = 25
+
+full_name = first_name +' '+last_name
+# print(full_name)
+
+# print(first_name[0])
+
+text = f'My name is {first_name} {last_name} . I am {age} years old.'
+# print(text)
+
+# for letter in first_name:
+#     # print(letter)       // print letter seperate line
+#     print(letter,end=' ')  // print letter same line
+
+# print(len(last_name))
+
+
+
+
