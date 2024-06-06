e7b0e526e7fa83e872bcac3091411169a8b91c6c
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-06-06 23:02:20 +0600

    Some string operation added

--- strings/strings.py
+++ strings/strings.py
@@ -1,6 +1,7 @@
 # string operations
 
 first_name = "Shahidul"
+# print(len(first_name))
 last_name = "Islam"
 age = 25
 
@@ -17,6 +18,26 @@ text = f'My name is {first_name} {last_name} . I am {age} years old.'
 #     print(letter,end=' ')  // print letter same line
 
 # print(len(last_name))
+
+
+a = """lorem ipsum 
+200"""       #Multi Line string 
+# print(a)
+
+txt = "The best thing is not free in life"
+
+# print('free' in txt)      # return true
+
+# print('Expensive' not in txt)   # return true
+
+item_no = 1505
+quantity = 10
+
+# myOrder = "I need item_no {} to {} piece"
+# print(myOrder.format(item_no,quantity))
+
+myOrder = "I need item_no {1} to {0} piece"
+# print(myOrder.format(quantity,item_no))   # you can set by indexing
 
 
 
